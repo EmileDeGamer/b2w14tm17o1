4f3ed1327722b36dd98733e91092cefceed99e6f
EmileDeGamer <emilemolzoho@gmail.com>
2019-11-13 12:42:55 +0100

    changes

--- js/test.js
+++ js/test.js
@@ -3,8 +3,9 @@ let check = document.getElementById('check')
 let UIWord = document.getElementById('UIWord')
 let word, lettersWord = [], lettersUIWord = [], gaps = [], tries = 5, gapsAmount = 25
 let guessedLetters = []
-let tempLetters = []
+/*let tempGoodLetters = [], */let tempIncludedLetters = []
 let tempFinal = []
+let guessed = "false"
 
 check.onclick = function(){checking()}
 
@@ -21,7 +22,8 @@ function setup(){
     }
     gaps[0].innerHTML = lettersWord[0]
     for (let i = 0; i < lettersWord.length; i++) {
-        tempLetters.push(lettersWord[i])
+        //tempGoodLetters.push(lettersWord[i])
+        tempIncludedLetters.push(lettersWord[i])
         guessedLetters.push("")
         tempFinal.push("false")
     }
@@ -32,20 +34,25 @@ function setup(){
 function checking(){
     if (UIWord.value.length == 5){
         console.clear()
-        lettersUIWord = UIWord.value.split("")
+        lettersUIWord = UIWord.value.toLowerCase().split("")
         for (let i = 0; i < lettersWord.length; i++) {
             tempFinal[i] = "false"
-            tempLetters[i] = lettersWord[i]
-        }
-        //console.log(lettersUIWord + " " + tempLetters)
-        if (words[word] == UIWord.value){
-            for (let i = 0; i == lettersWord.length; i++) {
+            //tempGoodLetters[i] = lettersWord[i]
+            tempIncludedLetters[i] = lettersWord[i]
+            if (words[word] == UIWord.value){
                 gaps[i].innerHTML = lettersUIWord[i]
                 gaps[i].className = "rechthoek"
                 check.disabled = true
+                guessed = "true"
             }
         }
-        if(check.disabled != true){
+        if (guessed == "true"){
+            alert("Juist! :D Woord: " + words[word])
+            location.reload()
+        }
+        //console.log(lettersUIWord + " " + tempLetters)
+        //console.log(guessed)
+        if (guessed == "false"){
             checkGoodPlace()
             checkWrongPlace()
             checkWrongLetter()
@@ -64,6 +71,10 @@ function checkTries(){
         UIWord.value = words[word]
         check.disabled = true
     }
+    if (tries == 0){
+        alert("Het woord is: " + words[word])
+        location.reload()
+    }
 }
 
 function checkGoodPlace(){
@@ -72,33 +83,43 @@ function checkGoodPlace(){
             if(guessedLetters[i] == ""){
                 guessedLetters[i] = lettersUIWord[i]
             }
-            tempLetters[i] = ""
+            tempIncludedLetters[i] = ""
+            //tempGoodLetters[i] = ""
             gaps[i].innerHTML = lettersUIWord[i]
             gaps[i].className = "rechthoek"
             //console.log(tempLetters)
             //console.log(guessedLetters)
             tempFinal[i] = "true"
             //console.log(tempFinal)
+            //console.log(tempGoodLetters)
+            //console.log(tempIncludedLetters)
         }
     }
 }
 
 function checkWrongPlace(){
     for (let i = 0; i < lettersWord.length; i++) {
-        if (tempLetters.includes(lettersUIWord[i]) && lettersUIWord[i] != lettersWord[i]){
+        /*if (tempGoodLetters.includes(lettersUIWord[i]) && lettersUIWord[i] != lettersWord[i] || */if(tempIncludedLetters.includes(lettersUIWord[i]) && lettersUIWord[i] != lettersWord[i]){ //&& tempIncludedLetters.includes(lettersUIWord[i])){
             //console.log(tempLetters[i])
             //console.log(tempLetters)
+            //tempIncludedLetters.remove(lettersUIWord[i])
+            let item = tempIncludedLetters.indexOf(lettersUIWord[i])
+            tempIncludedLetters.splice(item, 1, "")
+            //tempGoodLetters[i] = ""
             gaps[i].innerHTML = lettersUIWord[i]
             gaps[i].className = "rondje"
             tempFinal[i] = "true"
             //console.log(tempFinal)
+            //console.log(tempGoodLetters)
+            //console.log(tempIncludedLetters)
+            //console.log(tempIncludedLetters)
         }
     }
 }
 
 function checkWrongLetter(){
     for (let i = 0; i < lettersWord.length; i++) {
-        if (!tempLetters.includes(lettersUIWord[i]) && tempFinal[i] !== "true"){
+        /*if (!tempGoodLetters.includes(lettersUIWord[i]) && tempFinal[i] !== "true" || */if(!tempIncludedLetters.includes(lettersUIWord[i]) && tempFinal[i] !== "true"){ //&& !tempIncludedLetters.includes(lettersUIWord[i])){
             //console.log(tempLetters[i])
             //console.log(tempLetters)
             gaps[i].innerHTML = lettersUIWord[i]
@@ -112,6 +133,7 @@ function addGuessedLetters(){
         if (tries > 1){
             gaps[i + 5].innerHTML = guessedLetters[i]
         }
+        
         /*else if (tries == 1){
             gaps[i].innerHTML = 
         }*/
